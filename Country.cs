c334a7ecf0086a7a849b74f9ea5a1ae38c22eaff
Stoyan Kirov <stoqn_kirov10@abv.bg>
2020-07-02 16:24:07 +0300

    Add solution

---
+++ Country.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AssessmentTask
+{
+    public class Country
+    {
+        public string CountryName { get; }
+        public double AverageScore {
+            get
+            {
+                return Math.Round(Scores.Average(), 2);
+            }
+        }
+        private double MedianScore
+        {
+            get
+            {
+                this.Scores.Sort();
+                int count = Scores.Count();
+                if(count % 2 == 0)
+                {
+                    return (Scores[count / 2] + Scores[count / 2 - 1]) / 2;
+                }
+                else
+                {
+                    return Scores[count / 2];
+                }
+            }
+        }
+        private int MaxScore { get; set; }
+        private string MaxScorePerson { get; set; }
+        private int MinScore { get; set; }
+        private string MinScorePerson { get; set; }
+        private int RecordCount { get; set; }
+
+        private List<int> Scores = new List<int>();
+
+        public Country(string countryName, string firstName, string lastName, int score)
+        {
+            this.CountryName = countryName;
+            this.MaxScore = score;
+            this.MaxScorePerson = $"{firstName} {lastName}";
+            this.MinScore = score;
+            this.MinScorePerson = $"{firstName} {lastName}";
+            this.RecordCount = 1;
+            this.Scores.Add(score);
+        }
+
+        public void AddScore(string firstName, string lastName, int score)
+        {
+            this.Scores.Add(score);
+            if(this.MaxScore < score)
+            {
+                this.MaxScore = score;
+                this.MaxScorePerson = $"{firstName} {lastName}";
+            }
+            if(this.MinScore > score)
+            {
+                this.MinScore = score;
+                this.MinScorePerson = $"{firstName} {lastName}";
+            }
+            this.RecordCount++;
+        }
+
+        public string GetCountryInCsv()
+        {
+            return $"{CountryName};{AverageScore.ToString()};{MedianScore.ToString()};{MaxScore.ToString()};{MaxScorePerson};{MinScore.ToString()};{MinScorePerson};{RecordCount.ToString()}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+
+namespace AssessmentTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName, senderEmail, senderPassword, receiverEmail;
+
+            Console.WriteLine("Enter File name:");
+            fileName = Console.ReadLine();
+            Console.WriteLine("Enter Sender email address:");
+            senderEmail = Console.ReadLine();
+            Console.WriteLine("Enter Sender password:");
+            senderPassword = Console.ReadLine();
+            Console.WriteLine("Enter Receiver email address:");
+            receiverEmail = Console.ReadLine();
+
+            string line;
+            List<Country> Countries = new List<Country>();
+
+            try
+            {
+                StreamReader file = new StreamReader(fileName);
+
+                file.ReadLine();
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] data = line.Split(';');
+
+                    if (!Countries.Any(c => c.CountryName == data[2]))
+                    {
+                        Country newCountry = new Country(data[2], data[0], data[1], Int32.Parse(data[4]));
+                        Countries.Add(newCountry);
+                    }
+                    else
+                    {
+                        foreach (Country existingCountry in Countries)
+                        {
+                            if (existingCountry.CountryName == data[2])
+                            {
+                                existingCountry.AddScore(data[0], data[1], Int32.Parse(data[4]));
+                            }
+                        }
+                    }
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("File not found.Please try again.");
+                //Console.WriteLine(e.ToString());
+            }
+            
+
+            var sortedCountries = Countries.OrderByDescending(x => x.AverageScore).ToList();
+
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "ReportByCountry.csv");
+
+            using (StreamWriter fs = File.CreateText(path))
+            {
+                fs.WriteLine("Country;Average score;Median score;Max score;Max score person;Min score;Min score person;Record count");
+                foreach (Country c in sortedCountries)
+                {
+                    fs.WriteLine(c.GetCountryInCsv());
+                }
+            }
+
+            try
+            {            
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress(senderEmail);
+                mail.To.Add(receiverEmail);
+                mail.Subject = "Attached file";
+                mail.Body = "Mail with attachment";
+
+                Attachment attachment;
+                attachment = new Attachment(path);
+                mail.Attachments.Add(attachment);
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("Mail sent");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Incorrect Sender email address/password or Recepient email address.Please try again.");
+                //Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
